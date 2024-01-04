81bd01a4459e76ed895d388d6aa83416f3c54274
Xinyi Ouyang <oyxyddd@gamil.com>
2024-01-04 15:44:56 -0500

    Finished Part 3

--- ALAB308A.2.js
+++ ALAB308A.2.js
@@ -46,12 +46,44 @@ class Character {
         }
   }
 
-const robin = new Character("Robin");
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+// robin.companion.roll()
+
+//Part 3: Class Features
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+class Companion extends Character{
+    constructor (name,type) {
+        super(name);
+        this.type = type;
+      } 
+}
+
+const robin = new Adventurer("Robin","adventurer")
 robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
+robin.companion = new Companion("Leo","Cat");
+robin.companion.companion = new Companion("Frank","Flea");
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-robin.companion.roll()
+console.log(robin)
+
